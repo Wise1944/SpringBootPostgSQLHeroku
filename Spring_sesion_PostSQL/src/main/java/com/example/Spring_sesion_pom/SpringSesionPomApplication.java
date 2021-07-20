eedd58d2553d0e66df03f2631b7c90880747d1da
Jinnee <wise1944@yandex.ru>
2021-07-20 10:13:34 +0400

    Add files via upload

---
+++ Spring_sesion_PostSQL/src/main/java/com/example/Spring_sesion_pom/SpringSesionPomApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_sesion_pom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSesionPomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSesionPomApplication.class, args);
+	}
+
+}
---
+++ Spring_sesion_PostSQL/src/test/java/com/example/Spring_sesion_pom/SpringSesionPomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_sesion_pom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSesionPomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
